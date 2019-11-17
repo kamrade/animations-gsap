42b1fc1065d12fea2b78727b253b8be06954d0f5
Denis Michailov <kamrad.reg@gmail.com>
2019-11-17 18:45:55 +1000

    Update cardpay gsap

--- src/pages/cardpay-gsap.jsx
+++ src/pages/cardpay-gsap.jsx
@@ -7,35 +7,55 @@ import './cardpay-gsap.css';
 class CardpayGsapPage extends Component {
 
   stateIn;
+  stateIn1;
+  stateIn2;
 
   constructor(props) {
     super(props);
     this.state = {
-      stateIn: false
+      stateIn1: false,
+      stateIn2: false,
     }
     this.stateIn = false;
+    this.stateIn1 = false;
+    this.stateIn2 = false;
   }
 
   componentDidMount() {
-    this.tl = gsap.timeline({paused: true});
-    this.tl
-      .staggerFrom('.hidetext', 0.4, {
+
+    this.tl1 = gsap.timeline({paused: true});
+    this.tl1
+      .staggerFrom('.hidetext-1', 0.4, {
+        y: "200%",
+        ease: Power4.easeOut
+      }, 0.15);
+
+
+    this.tl2 = gsap.timeline({paused: true});
+    this.tl2
+      .staggerFrom('.hidetext-2', 0.4, {
         y: "200%",
         ease: Power4.easeOut
       }, 0.15);
   }
 
-  animateIn() {
-    if (!this.stateIn) {
-      this.stateIn = true;
-      this.tl.play();
+  animateIn(n) {
+    let currentSlideState = `stateIn${n}`
+    let currentAnimation  = `tl${n}`
+    if (!this[currentSlideState]) {
+      console.log('animate in', n);
+      this[currentSlideState] = true;
+      this[currentAnimation].play();
     }
   }
 
-  animateOut() {
-    if (this.stateIn) {
-      this.stateIn = false;
-      this.tl.reverse();
+  animateOut(n) {
+    let currentSlideState = `stateIn${n}`
+    let currentAnimation  = `tl${n}`
+    if (this[currentSlideState]) {
+      console.log('animate out', n);
+      this[currentSlideState] = false;
+      this[currentAnimation].reverse();
     }
   }
 
@@ -50,21 +70,41 @@ class CardpayGsapPage extends Component {
         <Controller>
           <Scene triggerElement="#trigger-1" indicators={true} duration={200}>
             { progress => {
-              if (progress > 0 && progress < 1) { this.animateIn(); }
-              else if (progress === 1 || progress <= 0) { this.animateOut(); }
+              if (progress > 0 && progress < 1) { this.animateIn(1); }
+              else if (progress === 1 || progress <= 0) { this.animateOut(1); }
               return (<div></div>)
             }}
           </Scene>
         </Controller>
 
         <div className="original-scene-title">
-          <h3><span className="hidetext">10 years</span></h3>
-          <h3><span className="hidetext">of experience</span></h3>
-          <h3><span className="hidetext">in fintech</span></h3>
+          <h3><span className="hidetext-1">10 years</span></h3>
+          <h3><span className="hidetext-1">of experience</span></h3>
+          <h3><span className="hidetext-1">in fintech</span></h3>
+        </div>
+
+        <div className="section section-separator" />
+        <div id="trigger-2" />
+
+        <Controller>
+          <Scene triggerElement="#trigger-2" indicators={true} duration={200}>
+            { progress => {
+              if (progress > 0 && progress < 1) { this.animateIn(2); }
+              else if (progress === 1 || progress <= 0) { this.animateOut(2); }
+              return (<div></div>)
+            }}
+          </Scene>
+        </Controller>
+
+        <div className="original-scene-title">
+          <h3><span className="hidetext-2">Fast onboarding</span></h3>
+          <h3><span className="hidetext-2">in 1 day</span></h3>
         </div>
 
         <div className="section" />
-        <div id="trigger-2" />
+        <div id="trigger-3" />
+
+
 
       </div>
     );
